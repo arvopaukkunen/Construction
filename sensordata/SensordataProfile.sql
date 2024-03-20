39d1748902065bdac7277087d2f564d98eee988f
Arvo Paukkunen <arvosoft@hotmail.com>
2024-03-20 22:03:43 +0200

    some more profiles...

--- sensordata/SensordataProfile.sql
+++ sensordata/SensordataProfile.sql
@@ -1,14 +1,35 @@
 --HUMIDITY
-SELECT name, value, description, manufactorer, create_time, CURRENT_TIMESTAMP as update_time, enabled, status, 'mySQLOnPrem' as source, 'humidy' as measure_type
+SELECT name, value, description, manufactorer, create_time, CURRENT_TIMESTAMP as update_time, 1 enabled, 'Active' status, 'mySQLOnPrem' as source, 'humidy' as measure_type
 FROM `sensor`
 where description = '%' and name like '%humidity%';
-
+--UNION ALL
 --BATTERY LEVEL 
-SELECT name, value, description, manufactorer, create_time, CURRENT_TIMESTAMP as update_time, enabled, status, 'mySQLOnPrem' as source, 'batterylevel' as measure_type
+SELECT name, value, description, manufactorer, create_time, CURRENT_TIMESTAMP as update_time, 1 enabled, 'Activve' status, 'mySQLOnPrem' as source, 'batterylevel' as measure_type
 FROM `sensor`
 where description = '%' and name like '%battery%';
-
---
-SELECT name, value, description, manufactorer, create_time, CURRENT_TIMESTAMP as update_time, enabled, status, 'mySQLOnPrem' as source, 'batterylevel' as measure_type
+--UNION ALL
+--STORAGE (% available)
+SELECT name, value, description, 'Apple' manufactorer, create_time, CURRENT_TIMESTAMP as update_time, 1 enabled, 'Active' status, 'mySQLOnPrem' as source, 'storege % left' as measure_type
 FROM `sensor`
-where description = '%' and name not like '%battery%' and name not like '%battery%' and name not like '%humidity%';
+where description = '% available' and name like '%storage%';
+--POWER (W)
+SELECT name, value, description, '' manufactorer, create_time, CURRENT_TIMESTAMP as update_time, 1 enabled, 'Active' status, 'mySQLOnPrem' as source, 'Consumption Power reading' as measure_type
+FROM `sensor`
+where description = 'W' --and name like '%storage%';
+--Noice (dB)
+SELECT name, value, description, '' manufactorer, create_time, CURRENT_TIMESTAMP as update_time, 1 enabled, 'Active' status, 'mySQLOnPrem' as source, 'Noise dB' as measure_type
+FROM `sensor`
+where description = 'dB'
+--Floors Ascended
+SELECT name, value, description, '' manufactorer, create_time, CURRENT_TIMESTAMP as update_time, 1 enabled, 'Active' status, 'mySQLOnPrem' as source, 'Noise dB' as measure_type
+FROM `sensor`
+where description = 'floors'
+--°C
+SELECT name, value, description, '' manufactorer, create_time, CURRENT_TIMESTAMP as update_time, 1 enabled, 'Active' status, 'mySQLOnPrem' as source, 'Temperature Celsius' as measure_type
+FROM `sensor`
+where description = '°C'
+-- ruuvitag_0c90_pressure 
+--
+SELECT name, value, description, manufactorer, create_time, CURRENT_TIMESTAMP as update_time, enabled, status, 'mySQLOnPrem' as source, 'Floors Ascended' as measure_type
+FROM `sensor`
+where description <> '%' and description <> '% available' and description <> 'W' and description <> 'dB' and description <> 'floors' and description <> '°C'
